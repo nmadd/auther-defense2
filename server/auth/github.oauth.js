6fc413daf84eea8a462d3407e564ac274a085210
nmadd <nate@fratmusic.com>
2015-12-11 16:13:58 -0500

    Initialize

---
+++ server/auth/github.oauth.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var router = require('express').Router();
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+
+var User = require('../api/users/user.model');
+
+router.get('/', passport.authenticate('github'));
+
+router.get('/callback', passport.authenticate('github', {
+	successRedirect: '/stories',
+	failureRedirect: '/signup'
+}));
+
+passport.use(new GitHubStrategy({
+	clientID: process.env.GITHUB_CLIENTID,
+	clientSecret: process.env.GITHUB_CLIENTSECRET,
+	callbackURL: process.env.GITHUB_CALLBACKURL
+}, function (token, refreshToken, profile, done) { 
+	User.findOne({'github.id': profile.id }, function (err, user) {
+		if (err) done(err);
+		else if (user) done(null, user);
+		else {
+			var email = profile.emails ? profile.emails[0].value : (profile.displayName.replace(/\s+/g, '_') + '@fake-auther-email.com');
+			var email = profile.emails[0].value;
+			User.create({
+				email: email,
+				name: profile.displayName,
+				github: {
+					id: profile.id,
+					name: profile.displayName,
+					email: email,
+					token: token
+				}
+			}, done);
+		}
+	});
+}));
+
+module.exports = router;
